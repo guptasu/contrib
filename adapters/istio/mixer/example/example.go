ba50ee4f8b3279efadab2374603337da5cdfda22
Douglas Reid <douglas-reid@users.noreply.github.com>
2018-01-10 20:20:10 -0800

    Add example Mixer adapter to extensions repo (#2)
    * Initial commit of example adapter with custom template
    
    * Add kube config
    
    * Add tests
    
    * Move to istio vendor directory

---
+++ adapters/istio/mixer/example/example.go
@@ -0,0 +1,115 @@
+// Copyright 2018 Istio Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate $GOPATH/src/istio.io/extensions/bin/codegen.sh -f adapters/istio/mixer/example/config/config.proto
+//go:generate $GOPATH/src/istio.io/extensions/bin/codegen.sh -t adapters/istio/mixer/example/template/template.proto
+
+// Package tmpl provides an tmpl Mixer adapter implementation. It is used
+// to provide code samples for future adapter developers on how to structure
+// their code and build scripts. It is also intended to be used in automated
+// Mixer testing to ensure that Mixer can be built and extended by adapters in
+// the extensions repo (as well as other external repos).
+//
+// The adapter implementation in this package handles instances from existing
+// Mixer templates: checknothing and reportnothing. For each
+// template, the adapter logs the request, including provided params.
+package example
+
+import (
+	"context"
+	"time"
+
+	"istio.io/istio/mixer/pkg/adapter"
+	"istio.io/istio/mixer/pkg/status"
+	"istio.io/istio/mixer/template/checknothing"
+	"istio.io/istio/mixer/template/reportnothing"
+
+	"istio.io/extensions/adapters/istio/mixer/example/config"
+	tmpl "istio.io/extensions/adapters/istio/mixer/example/template"
+)
+
+const (
+	validDuration = 10 * time.Second
+	validUses     = 1000
+)
+
+type (
+	builder struct {
+		cfg *config.Params
+	}
+
+	handler struct {
+		prefix string
+		log    adapter.Logger
+	}
+)
+
+// Ensure support all required interfaces.
+var (
+	_ tmpl.HandlerBuilder = &builder{}
+	_ tmpl.Handler        = &handler{}
+
+	_ checknothing.HandlerBuilder = &builder{}
+	_ checknothing.Handler        = &handler{}
+
+	_ reportnothing.HandlerBuilder = &builder{}
+	_ reportnothing.Handler        = &handler{}
+)
+
+// GetInfo returns the adapter.Info associated with this implementation.
+func GetInfo() adapter.Info {
+	return adapter.Info{
+		Name:        "example",
+		Impl:        "istio.io/extensions/adapter/mixer/example",
+		Description: "An example mixer adapter to showcase adapter development.",
+		SupportedTemplates: []string{
+			checknothing.TemplateName,
+			reportnothing.TemplateName,
+			tmpl.TemplateName,
+		},
+		DefaultConfig: &config.Params{LogPrefix: "mixer example adapter"},
+		NewBuilder:    func() adapter.HandlerBuilder { return &builder{} },
+	}
+}
+
+func (*builder) SetCheckNothingTypes(map[string]*checknothing.Type)   {}
+func (*builder) SetReportNothingTypes(map[string]*reportnothing.Type) {}
+func (*builder) SetExampleReportTypes(map[string]*tmpl.Type)          {}
+func (b *builder) SetAdapterConfig(cfg adapter.Config)                { b.cfg = cfg.(*config.Params) }
+func (*builder) Validate() (ce *adapter.ConfigErrors)                 { return }
+
+func (b *builder) Build(context context.Context, env adapter.Env) (adapter.Handler, error) {
+	return &handler{prefix: b.cfg.LogPrefix, log: env.Logger()}, nil
+}
+
+func (h *handler) HandleCheckNothing(ctx context.Context, instance *checknothing.Instance) (adapter.CheckResult, error) {
+	h.log.Infof("%s: received checknothing instance: %#v", h.prefix, instance)
+	return adapter.CheckResult{status.OK, validDuration, validUses}, nil
+}
+
+func (h *handler) HandleReportNothing(ctx context.Context, instances []*reportnothing.Instance) error {
+	for _, instance := range instances {
+		h.log.Infof("%s: received reportnothing instance: %#v", h.prefix, instance)
+	}
+	return nil
+}
+
+func (h *handler) HandleExampleReport(ctx context.Context, instances []*tmpl.Instance) error {
+	for _, instance := range instances {
+		h.log.Infof("%s: received example instance: %#v", h.prefix, instance)
+	}
+	return nil
+}
+
+func (*handler) Close() error { return nil }
---
+++ adapters/istio/mixer/example/example_test.go
@@ -0,0 +1,84 @@
+// Copyright 2018 Istio Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package example
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	tmpl "istio.io/extensions/adapters/istio/mixer/example/template"
+	"istio.io/istio/mixer/pkg/adapter/test"
+	"istio.io/istio/mixer/pkg/status"
+	"istio.io/istio/mixer/template/checknothing"
+	"istio.io/istio/mixer/template/reportnothing"
+)
+
+func TestValidateBuild(t *testing.T) {
+	b := GetInfo().NewBuilder().(*builder)
+	b.SetAdapterConfig(GetInfo().DefaultConfig)
+
+	if err := b.Validate(); err != nil {
+		t.Errorf("Validate() resulted in unexpected error: %v", err)
+	}
+
+	// invoke the empty set methods for coverage
+	b.SetCheckNothingTypes(map[string]*checknothing.Type{})
+	b.SetReportNothingTypes(map[string]*reportnothing.Type{})
+	b.SetExampleReportTypes(map[string]*tmpl.Type{})
+
+	if _, err := b.Build(context.Background(), test.NewEnv(t)); err != nil {
+		t.Errorf("Build() resulted in unexpected error: %v", err)
+	}
+}
+
+func TestHandler(t *testing.T) {
+	ctx := context.Background()
+	testEnv := test.NewEnv(t)
+	h := &handler{prefix: "Test", log: testEnv.Logger()}
+
+	got, err := h.HandleCheckNothing(ctx, nil)
+	if err != nil {
+		t.Errorf("HandleCheckNothing(ctx, nil) resulted in an unexpected error: %v", err)
+	}
+	if !status.IsOK(got.Status) {
+		t.Errorf("HandleCheckNothing(ctx, nil) => %#v, want %#v", got.Status, status.OK)
+	}
+
+	rni := &reportnothing.Instance{"rni"}
+	if err := h.HandleReportNothing(ctx, []*reportnothing.Instance{rni}); err != nil {
+		t.Errorf("HandleReportNothing(ctx, []{%#v}) resulted in an unexpected error: %v", rni, err)
+	}
+
+	ei := &tmpl.Instance{"ei", map[string]interface{}{"foo": "bar"}}
+	if err := h.HandleExampleReport(ctx, []*tmpl.Instance{ei}); err != nil {
+		t.Errorf("HandleExampleReport(ctx, []{%#v}) resulted in an unexpected error: %v", ei, err)
+	}
+
+	logs := testEnv.GetLogs()
+	if len(logs) != 3 {
+		t.Errorf("Got %d logs; want %d", len(logs), 3)
+	}
+
+	for _, l := range logs {
+		if !strings.HasPrefix(l, "Test:") {
+			t.Errorf("Log does not start with expected prefix, want 'Test:': %s", l)
+		}
+	}
+
+	if err := h.Close(); err != nil {
+		t.Errorf("Close() returned an unexpected error")
+	}
+}
